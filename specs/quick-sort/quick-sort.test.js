deb4e5039a72b77c9dab24e3cc5c74e83be083eb
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-11-05 19:09:26 +0530

    Add Quick Sort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -14,11 +14,35 @@
 
 function quickSort(nums) {
   // code goes here
+  // base case, array of length 0 or 1
+  if (nums.length <= 1) return nums;
+
+  // choose pivot
+  const pivot = nums[nums.length - 1];
+
+  // separate into left and right arrrays
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < pivot) {
+      left.push(nums[i]);
+    } else {
+      right.push(nums[i]);
+    }
+  }
+  // call quickSort on left and right arrays
+  // const sortedLeft = quickSort(left);
+  // const sortedRight = quickSort(right);
+
+  // return left.concat(pivot, right)
+  // return sortedLeft.concat(pivot, sortedRight);
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
