5ec39b629908c1d8b23c6925889ef09805ead510
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-11-08 10:18:28 +0530

    Add AVL Trees

--- src/Tree.js
+++ src/Tree.js
@@ -8,39 +8,13 @@ class Tree {
   constructor() {
     this.root = null;
   }
-
   add(value) {
-    // some logic around if this is the root
-    if (this.root === null) {
+    if (!this.root) {
       this.root = new Node(value);
     } else {
-      let current = this.root;
-      while (true) {
-        if (current.value > value) {
-          // go left
-
-          if (current.left) {
-            current = current.left;
-          } else {
-            current.left = new Node(value);
-            break;
-          }
-        } else {
-          // go right
-
-          if (current.right) {
-            current = current.right;
-          } else {
-            current.right = new Node(value);
-            break;
-          }
-        }
-      }
+      this.root.add(value);
     }
-    // find the correct place to add
-    return this;
   }
-
   toJSON() {
     return JSON.stringify(this.root.serialize(), null, 4);
   }
@@ -49,25 +23,131 @@ class Tree {
   }
 }
 
-// you might consider using a Node class too
 class Node {
-  // code maybe goes here
+  // code also goes here
   constructor(value = null, left = null, right = null) {
-    this.value = value;
     this.left = left;
     this.right = right;
+    this.value = value;
+    this.height = 1;
+  }
+  add(value) {
+    // decide to go left or right
+
+    // find the correct place to add
+
+    // make sure you're updating heights
+
+    if (value < this.value) {
+      // go left
+      if (this.left) {
+        this.left.add(value);
+      } else {
+        this.left = new Node(value);
+      }
+      if (!this.right || this.right.height < this.left.height) {
+        this.height = this.left.height + 1;
+      }
+    } else {
+      // go right
+      if (this.right) {
+        this.right.add(value);
+      } else {
+        this.right = new Node(value);
+      }
+      if (!this.left || this.right.height > this.left.height) {
+        this.height = this.right.height + 1;
+      }
+    }
+
+    this.balance();
+  }
+
+  balance() {
+    const rightHeight = this.right ? this.right.height : 0;
+    const leftHeight = this.left ? this.left.height : 0;
+
+    if (leftHeight > rightHeight + 1) {
+      const leftRightHeight = this.left.right ? this.left.right.height : 0;
+      const leftLeftHeight = this.left.left ? this.left.left.height : 0;
+
+      // ask is this node out of balance
+      // double rotation
+      if (leftRightHeight > leftLeftHeight) {
+        this.left.rotateRR();
+      }
+
+      this.rotateLL();
+    } else if (rightHeight > leftHeight + 1) {
+      const rightRightHeight = this.right.right ? this.right.right.height : 0;
+      const rightLeftHeight = this.right.left ? this.right.left.height : 0;
+
+      // double rotation
+      if (rightLeftHeight > rightRightHeight) {
+        this.right.rotateLL();
+      }
+
+      this.rotateRR();
+    }
+    // if not out of balance, do nothing
+    // if it is out of balance, do I need to single or double  rotate
+    // if single, just call rotate on self
+    // if double, call rotate on child them on self
+  }
+
+  rotateRR() {
+    // rotate
+    const valueBefore = this.value;
+    const leftBefore = this.left;
+    this.value = this.right.value;
+    this.left = this.right;
+    this.right = this.right.right;
+    this.left.right = this.left.left;
+    this.left.left = leftBefore;
+    this.left.value = valueBefore;
+    this.left.updateInNewLocation();
+    this.updateInNewLocation();
+  }
+
+  rotateLL() {
+    // rotate
+    const valueBefore = this.value;
+    const rightBefore = this.right;
+    this.value = this.left.value;
+    this.right = this.left;
+    this.left = this.left.left;
+    this.right.left = this.right.right;
+    this.right.right = rightBefore;
+    this.right.value = valueBefore;
+    this.right.updateInNewLocation();
+    this.updateInNewLocation();
+  }
+
+  updateInNewLocation() {
+    // calculate the new height
+    if (!this.right && !this.left) {
+      this.height = 1;
+    } else if (
+      !this.right ||
+      (this.left && this.right.height < this.left.height)
+    ) {
+      this.height = this.left.height + 1;
+    } else {
+      this.height = this.right.height + 1;
+    }
   }
 
   serialize() {
     const ans = { value: this.value };
     ans.left = this.left === null ? null : this.left.serialize();
     ans.right = this.right === null ? null : this.right.serialize();
+    ans.height = this.height;
     return ans;
   }
 }
 
 export default function TreeComponent() {
-  const nums = _.shuffle(_.range(20));
+  const nums = _.shuffle(_.range(50));
   const tree = new Tree();
   nums.map((num) => tree.add(num));
   const objs = tree.toObject();
